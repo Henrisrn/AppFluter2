692b19c456e125a27d57f9c183a06e13394ddd30
Henri Serano <srn.henri@gmail.com>
2022-08-25 14:58:13 +0200

    J12

--- myapp/lib/screens/home.dart
+++ myapp/lib/screens/home.dart
@@ -1,4 +1,8 @@
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,12 +21,77 @@ class _HomeState extends State<Home> {
             child:Text("Titre application")
             ),
         ),
-        body:Container(
-          child: Center(
-            child:Text("Coucou"),
+        body:Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              RichText(
+                text: TextSpan(
+                  text:"Bienvenue \n".toUpperCase(),
+                  style: TextStyle(
+                    color:Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text:"sur ma première application".toLowerCase(),
+                      style: TextStyle(
+                        fontSize: 15,
+                        color:Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold,
+                      )
+                  )],
+                ),
+              ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Alors ça a de la gueule",style: TextStyle(
+                fontStyle: FontStyle.italic, 
+              ),
+              ),
+            SizedBox(
+              height: 50,
+            ),
+            Form(child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text("Entrez votre email"),
+                SizedBox(height:10,),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40),
+                      borderSide: BorderSide(color: Colors.grey,)
+                    ),
+                    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(40),
+                      borderSide: BorderSide(color: Colors.grey,)),
+                  ),
+                ),
+                SizedBox(height: 10,)
+                ]),
+            ),
+            Center(child: RaisedButton(
+              onPressed: () => print("Appuyé"),
+              color: Theme.of(context).primaryColor,
+              
+              padding: EdgeInsets.symmetric(vertical: 10,),
+              child:Center(child:Text("Continue".toUpperCase(),
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              ),),
+              ),),
+          ],
           ),
-          color: Colors.white,
-        )
+          )
+        ),
+        backgroundColor: Colors.white,
       )
     );
   }
